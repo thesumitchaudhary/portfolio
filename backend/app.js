dbaeca0ce550a18c85a93b1892938fabbc759545
Sumit <160419898+thesumitchaudhary@users.noreply.github.com>
2026-01-15 23:57:59 +0530

    back end change

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors({
-  origin: process.env.ORIGIN || "http://localhost:5173",
+  origin: process.env.CORS_ORIGIN || "http://localhost:5173",
   credentials: true
 }));
 
